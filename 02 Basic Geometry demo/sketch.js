e78cfbf8b0875a7f2f5df32d0365e82e7d5b49c9
caydenhnatiuk <60625600+caydenhnatiuk@users.noreply.github.com>
2020-02-05 14:10:25 -0600

    Basic Geometry and events

---
+++ 02 Basic Geometry demo/sketch.js
@@ -0,0 +1,30 @@
+// basic geometry and events demo
+let x,y;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  y = height/2;
+}
+
+function draw() {
+  background(100);
+  character(x,y);
+}
+
+function character(x,y){
+  fill(255);
+  strokeWeight(1);
+  ellipse(x,y,50,50);//head
+  fill(0,200,0);
+  ellipse(x-10,y-10,10,10);//left eye
+  ellipse(x+10,y-10,10,10);//righteye
+  if (mouseIsPressed){
+    fill(0);
+    ellipse(x,y+10,10,10)
+  }
+  else{
+    strokeWeight(3);//mouth thickness
+    line(x-5,y+10,x+5,y+10);//mouth
+  }
+}
--- copy for thing/sketch.js
+++ copy for thing/sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +13,16 @@ function setup() {
 
 function draw() {
   background(220);
+  fill(r,0,0);
+  ellipse(mouseX - 40, mouseY -40, 40, 40)
+}
+
+function mousePressed(){
+  if (r === 0){
+    r=255;
+  
+  } 
+else {
+  r=0;
+}
 }
