47fd2fcf33c3137d1990d84015103842ff24ccbd
caydenhnatiuk <60625600+caydenhnatiuk@users.noreply.github.com>
2020-02-07 14:10:56 -0600

    not much change

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -15,12 +15,21 @@ function draw() {
   character();
 }
 
+function keyPressed(){
+  
+}
+
 function background1(){
+  let sunSize = 150;
+  if(keyCode === UP_ARROW){
+    sunSize = sunSize +1;
+    return sunSize;
+  }
   background(53,179,204);
   fill(123,252,3);
   rect(0,(windowHeight/4)*3,windowWidth,windowHeight);
   fill(240,252,3);
-  ellipse(windowWidth/5,windowHeight/5,150);
+  ellipse(windowWidth/5,windowHeight/5,sunSize,sunSize);
 }
 
 function character(){
@@ -30,7 +39,9 @@ function character(){
   fill(255,255,255);
   ellipse(mouseX-10,mouseY-45,10,10);//left eye
   ellipse(mouseX+10,mouseY-45,10,10);//right eye
+  fill(52,158,235);
   rect(mouseX-20,mouseY+105,10,50);//left leg
   rect(mouseX+10,mouseY+105,10,50);//right leg
+  fill(235,85,52);
   rect(mouseX-40,mouseY-15,80,120);//body
 }
