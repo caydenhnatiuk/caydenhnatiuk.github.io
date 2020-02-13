7138eacdec4c9efb80faee35d7c29bbfdd5d0e35
caydenhnatiuk <60625600+caydenhnatiuk@users.noreply.github.com>
2020-02-13 12:13:35 -0600

    rectangles

--- Rollover Effect Assignment/sketch.js
+++ Rollover Effect Assignment/sketch.js
@@ -2,15 +2,31 @@
 // Cayden Hnatiuk
 // Feb 12th
 
-x=mouseX;
-y=mouseY;
+let onLeft,onRight,inTop,inBottom;
+let leftFade = 255
+let rightFade = 255
+let topFade = 255
+let bottomFade = 255
+const FADE_SPEED = 3
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  onLeft= 
 }
 
 function draw() {
   background(220);
+  makeRectangles();
+}
+
+function makeRectangles(){
+  if (onLeft && inTop){
+    fill(0,0,0,leftFade)
+  }
+  else {
+    fill(0);
+    leftFade = 255
+  }
   rect(0,0, width/2,height/2);//Top Left
   rect(width/2,0,width/2,height/2);//Top Right
   rect(0,height/2,width/2,height/2);//Bottom Left
@@ -18,7 +34,10 @@ function draw() {
 }
 
 function cursorLocation(){
-  if(x < width/2 && y < height/2){
-    topLeft = true;
+  if(mouseX < width/2 && mouseY < height/2){//Top Left
+    onLeft = true;
+    inTop = true;
+    inBottom = false;
+    onRight = false;
   }
 }
