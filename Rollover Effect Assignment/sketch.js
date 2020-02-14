487ff4aaa238ff0d3adef69873e4f4d5d0d3d229
caydenhnatiuk <60625600+caydenhnatiuk@users.noreply.github.com>
2020-02-14 14:24:58 -0600

    quadrants

--- Rollover Effect Assignment/sketch.js
+++ Rollover Effect Assignment/sketch.js
@@ -3,15 +3,16 @@
 // Feb 12th
 
 let onLeft,onRight,inTop,inBottom;
-let leftFade = 255
-let rightFade = 255
-let topFade = 255
-let bottomFade = 255
-const FADE_SPEED = 3
+let q1 = 255;
+let q2 = 255;
+let q3 = 255;
+let q4 = 255;
+let q1Fill,q2Fill,q3Fill,q4Fill = 0;
+let quadrant = 1;
+const FADE_SPEED = 3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  onLeft= 
 }
 
 function draw() {
@@ -20,13 +21,13 @@ function draw() {
 }
 
 function makeRectangles(){
-  if (onLeft && inTop){
-    fill(0,0,0,leftFade)
+  if (quadrant === 1){
+    q1Fill = 255;
   }
   else {
-    fill(0);
-    leftFade = 255
+    q1Fill -= 10;
   }
+  fill(q1Fill);
   rect(0,0, width/2,height/2);//Top Left
   rect(width/2,0,width/2,height/2);//Top Right
   rect(0,height/2,width/2,height/2);//Bottom Left
@@ -35,9 +36,9 @@ function makeRectangles(){
 
 function cursorLocation(){
   if(mouseX < width/2 && mouseY < height/2){//Top Left
-    onLeft = true;
-    inTop = true;
-    inBottom = false;
-    onRight = false;
+    quadrant = 1;
+  }
+  else if(mouseX>width/2 && mouseY < height/2){//Top Right
+    quadrant = 2;
   }
 }
