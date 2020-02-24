6d40151de73af14f1e33bde7a48b6ca31bab6bbd
caydenhnatiuk <60625600+caydenhnatiuk@users.noreply.github.com>
2020-02-24 14:11:13 -0600

    quadrants done

--- Rollover Effect Assignment/sketch.js
+++ Rollover Effect Assignment/sketch.js
@@ -7,8 +7,11 @@ let q1 = 255;
 let q2 = 255;
 let q3 = 255;
 let q4 = 255;
-let q1Fill,q2Fill,q3Fill,q4Fill = 0;
-let quadrant = 1;
+let q1Fill=0;
+let q2Fill=0;
+let q3Fill=0;
+let q4Fill = 0;
+let quadrant = 0;
 const FADE_SPEED = 3;
 
 function setup() {
@@ -17,20 +20,34 @@ function setup() {
 
 function draw() {
   background(220);
+  cursorLocation();
   makeRectangles();
 }
 
 function makeRectangles(){
   if (quadrant === 1){
-    q1Fill = 255;
+    q1Fill = 0;
   }
-  else {
-    q1Fill -= 10;
-  }
+  else q1Fill += 10;
   fill(q1Fill);
   rect(0,0, width/2,height/2);//Top Left
+  if (quadrant === 2){
+    q2Fill = 0;
+  }
+  else q2Fill += 10;
+  fill(q2Fill);
   rect(width/2,0,width/2,height/2);//Top Right
+  if (quadrant === 3){
+    q3Fill = 0;
+  }
+  else q3Fill += 10;
+  fill(q3Fill);
   rect(0,height/2,width/2,height/2);//Bottom Left
+  if (quadrant === 4){
+    q4Fill = 0;
+  }
+  else q4Fill += 10;
+  fill(q4Fill);
   rect(width/2,height/2,width/2,height/2);//Bottom Right
 }
 
@@ -41,4 +58,10 @@ function cursorLocation(){
   else if(mouseX>width/2 && mouseY < height/2){//Top Right
     quadrant = 2;
   }
+  else if(mouseX < width/2 && mouseY > height/2){//Bottom Left
+    quadrant = 3;
+  }
+  else if(mouseX > width/2 && mouseY > height/2){// Bottom Right
+    quadrant = 4;
+  } 
 }
