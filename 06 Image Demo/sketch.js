4bddb14f520b5a06693e3ee036b3d1e887216c14
caydenhnatiuk <60625600+caydenhnatiuk@users.noreply.github.com>
2020-02-25 14:10:26 -0600

    demos

---
+++ 06 Image Demo/sketch.js
@@ -0,0 +1,44 @@
+//Image demo
+
+let lionL,lionR;
+let movingLeft = true;
+
+function preload(){
+  lionL = loadImage("assets/lion-left.png");
+  lionR = loadImage("assets/lion-right.png");
+}
+
+function determineDirection(){
+  if(pmouseX < mouseX){
+    //moving right
+    movingLeft = false;
+  }
+  else if (pmouseX > mouseX){
+    //moving left
+    movingLeft = true;
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  noCursor();
+}
+
+function draw() {
+  print(movingLeft);
+  background(220);
+  determineDirection();
+  if (keyIsPressed){
+    tint(50,100,100);
+  }
+  else{
+    noTint();
+  }
+  if(movingLeft){
+    image(lionL, mouseX,mouseY,130, 90);
+  }
+  else{
+    image(lionR, mouseX,mouseY,130, 90);
+  }
+}
